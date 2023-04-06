6eb1da70a7129de57954d639b33e0574f01cb82c
Brian Joo <brianhjoo@gmail.com>
2023-04-06 15:14:47 -0700

    finished part 1

--- routes/companies.js
+++ routes/companies.js
@@ -2,35 +2,114 @@ const express = require("express");
 
 const db = require("../db");
 const router = new express.Router();
-const { NotFoundError } = require("../expressError");
+const { NotFoundError, BadRequestError } = require("../expressError");
 
+
+/** GET - returns all companies
+ *
+ *  >> {companies: [{code, name}, ...]}
+ */
 router.get("", async function(req, res, next) {
-  //get all companies from db
+
   const result = await db.query(
     `SELECT code, name
-        FROM companies
-    `
-  )
-  const companies = result.rows;
-  // console.log("companies", companies);
-  //return all companies in json
-  return res.json({companies});
-})
+        FROM companies`
+  );
 
+  const companies = result.rows;
+
+  return res.json({companies});
+});
+
+
+/** GET - returns a single company
+ *
+ * >> {company: {code, name, description}}
+*/
 router.get("/:code", async function(req, res, next) {
   const companyCode = req.params.code;
-  //get all companies from db
+
   const result = await db.query(
     `SELECT code, name, description
         FROM companies
         WHERE code = $1`, [companyCode]);
   const company = result.rows[0];
-  console.log("company", company);
 
-  //if comp doesn't exist return 404
   if (result.rows.length === 0) throw new NotFoundError;
 
   return res.json({company});
-})
+});
+
+
+/** POST - creates a new company
+ *
+ * req: {code, name, description}
+ * >> {company: {code, name, description}}
+*/
+router.post("", async function(req, res, next) {
+  if (!req.body) throw new BadRequestError();
+
+  const { code, name, description } = req.body;
+
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`,
+    [code, name, description],    // TODO: ask about comma at end
+  );
+
+  const company = results.rows[0];
+
+  return res.status(201).json({company});
+});
+
+
+/** PUT - updates a company
+ *
+ * req: {name, description}
+ * >> {company: {code, name, description}}
+ */
+router.put('/:code', async function(req, res, next) {
+  if (!req.body) throw new BadRequestError();
+
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+        SET name=$1,
+            description=$2
+        WHERE code = $3
+        RETURNING code, name, description`,
+        [name, description, req.params.code],
+  );
+
+  const company = result.rows[0];
+
+  if (!company) throw new NotFoundError();
+
+  return res.json({company});
+});
+
+
+/** DELETE - deletes a company
+ *
+ * >> {status: "deleted"}
+ */
+router.delete('/:code', async function(req, res, next) {
+  const result = await db.query(
+    `DELETE FROM companies
+        WHERE code = $1
+        RETURNING code`,
+    [req.params.code],
+  );
+
+  const company = result.rows[0];
+
+  if (!company) throw new NotFoundError();
+
+  return res.json({ message: 'Deleted' });
+});
+
+
 
 module.exports = router;
