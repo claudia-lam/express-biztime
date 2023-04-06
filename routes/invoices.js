1310bc2520039199f7f6a7c13f1d8760a479ea62
Brian Joo <brianhjoo@gmail.com>
2023-04-06 16:54:34 -0700

    finished invoice post and put routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -79,5 +79,55 @@ router.get("/:id", async function(req, res, next) {
 });
 
 
+/**
+ *  POST - create a new invoice
+ *
+ *  >> {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.post('', async function(req, res, next) {
+  if (!req.body) throw new BadRequestError();
+
+  const { comp_code, amt } = req.body;
+
+  const results = await db.query(
+    `INSERT INTO invoices (comp_code, amt)
+        VALUES ($1, $2)
+        RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [comp_code, amt],
+  );
+
+  const invoice = results.rows[0];
+
+  return res.status(201).json({invoice});
+});
+
+
+/**
+ * PUT - updates a invoice
+ *
+ * >> {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.put('/:id', async function(req, res, next) {
+  if (!req.body) throw new BadRequestError();
+
+  const { amt } = req.body;
+
+  const result = await db.query(
+    `UPDATE invoices
+      SET amt=$1
+      WHERE id = $2
+      RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [amt, req.params.id],
+  );
+
+  const invoice = result.rows[0];
+
+  if (!invoice) throw new NotFoundError();
+
+  return res.json({invoice});
+});
+
+
+
 
 module.exports = router;
